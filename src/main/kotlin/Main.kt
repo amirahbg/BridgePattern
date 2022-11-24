186607c5502acdd4b05677b96fdb6a0a1bd96bfa
amir.bagheri <amirahbg@gmail.com>
2022-11-24 13:21:32 +0330

    add main function to start the app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+
+fun main() {
+    val stormTrooper = StormTrooper(Rifle(), RegularLegs())
+    val flameTrooper = StormTrooper(Flamethrower(), RegularLegs())
+    val scoutTrooper = StormTrooper(Rifle(), AthleticLegs())
+}
